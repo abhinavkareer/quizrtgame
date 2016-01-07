d21e3cef18c35ad04f6e81749bf738e998a51c1d
Sunil Mekala <sunil.rao15@gmail.com>
2016-01-07 16:50:46 +0530

    add template updation for other types of tournaments

--- js/tournaments-script.js
+++ js/tournaments-script.js
@@ -1,9 +1,9 @@
 (function($){
 	
 	$.getJSON('resources/tournament.json').done(function(data){
-		var template = $.trim($("#myTournmentsTemplate").html()),
+		var template = $.trim($("#myTournamentsTemplate").html()),
 						fragment="";
-		console.log(template);
+		
 
 		$.each( data.tournaments.playing, function( i, item ) {
 			fragment += template.replace(/{{name}}/ig , item.name)
@@ -12,7 +12,35 @@
 								.replace(/{{topic}}/ig , item.topic);
 		});
 
-		$("#myTournments").append(fragment);
+		
+
+		$("#myTournaments").append(fragment);
+		
+
+		template = $.trim($("#createdTournamentsTemplate").html());
+		fragment="";
+
+		$.each( data.tournaments.created, function( i, item ) {
+			fragment += template.replace(/{{name}}/ig , item.name)
+								.replace(/{{image}}/ig , "images/" +item.image)
+								.replace(/{{prize}}/ig , "$" +item.prize)
+								.replace(/{{topic}}/ig , item.topic);
+		});
+		console.log(fragment);
+
+		$("#created_tournaments").append(fragment);
+
+		template = $.trim($("#tournamentsHistoryTemplate").html());
+		fragment="";
+
+		$.each( data.tournaments.history, function( i, item ) {
+			fragment += template.replace(/{{name}}/ig , item.name)
+								.replace(/{{image}}/ig , "images/" +item.image)
+								.replace(/{{prize}}/ig , "$" +item.prize)
+								.replace(/{{topic}}/ig , item.topic);
+		});
+
+		$("#tournaments_history").append(fragment);
 	})
 	.fail(function( jqxhr, textStatus, error ) {
     var err = textStatus + ", " + error;
