b5ea45849c8791c48713a01b99214e64ec66e71f
Sunil Mekala <sunil.rao15@gmail.com>
2016-01-08 15:56:53 +0530

    added carousel feature for played torunaments

--- js/tournaments-script.js
+++ js/tournaments-script.js
@@ -1,5 +1,5 @@
 (function($){
-	
+	var tile_colors = ['#2672EC' , '#5133AB' , '#AC193D' , '#D24726' , '#008A00' , '#00A3A3'];
 	$.getJSON('resources/tournament.json').done(function(data){
 		var template = $.trim($("#myTournamentsTemplate").html()),
 						fragment="";
@@ -12,11 +12,16 @@
 								.replace(/{{topic}}/ig , item.topic);
 
 			if(i<3){
+				
 				$("div .item.active").append(fragment);
+				var temp = $("div .item.active div").last();
+				
+				//addClass('tile-'+ (i%6));
 				fragment ="";
 
 			}else if((i!= 0) && (i%3 == 0)){
 				var temp= $("<div class='item'></div>").append(fragment);
+				//temp.addClass('tile-'+ (i%6));
 				$("div .carousel-inner").append(temp);
 				
 				fragment = "";
