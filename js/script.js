e3efdea0ea197dc8d8ca97c12b47e887aa57ca34
Anil Sawant <anilsawant241191@gmail.com>
2016-01-08 22:17:37 +0530

    using tiles approach

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-(function () {
-  // var fs = require("fs");
-  //
-  // console.log("Started reading file " + file );
-  // var rawData = fs.readFileSync( file );
-  // console.log("Completed reading file " + file );
-  // rawData = rawData.toString();
-  // console.log(JSON.parse(rawData));
-  console.log("Started");
-  var file = "resources/user1_teams.json";
-  $.getJSON( file, function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
+(function($){
+	var tile_colors = ['#2672EC' , '#5133AB' , '#AC193D' , '#D24726' , '#008A00' , '#00A3A3'];
+	$.getJSON('resources/user1_teams.json').done( function(data) {
+    var rows = Object.keys(data),
+        len = rows.length;
+
+    for (var i = 0; i < len; i++) {
+      var rowTemplate = $( ('#' + rows[i]) );
+      if( rowTemplate ) {
+        var teamsInRow = data[ rows[i] ],
+            noOfTeams = teamsInRow.length,
+            rowTemplateHtml = $.trim(rowTemplate.html());
+
+        for (var j = 0; j < noOfTeams; j++) {
+          var teamTile = rowTemplateHtml;
+          teamTile = teamTile.replace(/{{image}}/ig, 'images/team-icons/team_default_64x64.png' )
+                             .replace(/{{name}}/ig, teamsInRow[j].teamName );
+
+          rowTemplate.closest('.row').append( teamTile );
+        }
+      }
+    }
   });
-console.log(data);
-  $( "<ul>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
-})();
-console.log("executing");
+})(jQuery);
