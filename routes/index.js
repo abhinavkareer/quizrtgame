467c509ef904a94af36ebecc466aa084a04f0707
unknown <abhinavkareer@gmail.com>
2016-01-14 02:08:37 -0800

    extra-header, extra-scripts blocks added to layout.jade

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,17 @@ var fs=require('fs');
 
 var router = express.Router();
 
+/* GET People page. */
+router.get('/people', function(req, res, next) {
+  res.render('people', { title: 'People|QuizRT Game World' });
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'QuizRT Game World' });
 });
-// router.get('/getHallOFFameJson', function(req, res, next) {
-// var countryContinentMap=JSON.parse(fs.readFileSync('./../public/resources/hallOfFame.json', 'utf8'));
-// console.log(countryContinentMap);
-// });
+router.get('/getHallOFFameJson', function(req, res, next) {
+  var countryContinentMap=JSON.parse(fs.readFileSync('./public/resources/hallOfFame.json', 'utf8'));
+  res.json(countryContinentMap);
+});
 
 module.exports = router;
